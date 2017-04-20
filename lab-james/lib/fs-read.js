7e7a234591bb5fe29e62f6051ffb35e3c65b6011
Ithalis <jt.2012@yahoo.com>
2017-04-20 11:41:48 -0700

    Finished one test

--- lab-james/lib/fs-read.js
+++ lab-james/lib/fs-read.js
@@ -3,17 +3,22 @@
 const fs = require('fs');
 module.exports = exports = {};
 
+exports.textFiles = [];
+
 exports.readFiles = function(){
   fs.readFile(`${__dirname}/../data/one.txt`, function(err, data){
     if(err) throw err;
-    console.log(data.toString('hex', 0, 8));
+    console.log(`One: ${data.toString('hex', 0, 8)}`);
+    exports.textFiles.push(data.toString('hex', 0, 8));
   });
   fs.readFile(`${__dirname}/../data/two.txt`, function(err, data){
     if(err) throw err;
-    console.log(data.toString('hex', 0, 8));
+    console.log(`Two: ${data.toString('hex', 0, 8)}`);
+    exports.textFiles.push(data.toString('hex', 0, 8));
   });
   fs.readFile(`${__dirname}/../data/three.txt`, function(err, data){
     if(err) throw err;
-    console.log(data.toString('hex', 0, 8));
+    console.log(`Three: ${data.toString('hex', 0, 8)}`);
+    exports.textFiles.push(data.toString('hex', 0, 8));
   });
 };
