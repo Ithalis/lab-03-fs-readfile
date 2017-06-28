ed2ac792e9e14f97b2f37f018ec8181fa760ec5b
James Thomas <jt.2012@yahoo.com>
2017-06-28 22:20:20 +0000

    Fixed async and finished tests

--- lab-james/lib/fs-read.js
+++ lab-james/lib/fs-read.js
@@ -6,19 +6,22 @@ module.exports = exports = {};
 exports.textFiles = [];
 
 exports.readFiles = function(){
+  
   fs.readFile(`${__dirname}/../data/one.txt`, function(err, data){
     if(err) throw err;
     console.log(`One: ${data.toString('hex', 0, 8)}`);
     exports.textFiles.push(data.toString('hex', 0, 8));
-  });
-  fs.readFile(`${__dirname}/../data/two.txt`, function(err, data){
-    if(err) throw err;
-    console.log(`Two: ${data.toString('hex', 0, 8)}`);
-    exports.textFiles.push(data.toString('hex', 0, 8));
-  });
-  fs.readFile(`${__dirname}/../data/three.txt`, function(err, data){
-    if(err) throw err;
-    console.log(`Three: ${data.toString('hex', 0, 8)}`);
-    exports.textFiles.push(data.toString('hex', 0, 8));
+
+    fs.readFile(`${__dirname}/../data/two.txt`, function(err, data){
+      if(err) throw err;
+      console.log(`Two: ${data.toString('hex', 0, 8)}`);
+      exports.textFiles.push(data.toString('hex', 0, 8));
+
+      fs.readFile(`${__dirname}/../data/three.txt`, function(err, data){
+        if(err) throw err;
+        console.log(`Three: ${data.toString('hex', 0, 8)}`);
+        exports.textFiles.push(data.toString('hex', 0, 8));
+      });  
+    });
   });
 };
--- lab-james/test/fs-read-test.js
+++ lab-james/test/fs-read-test.js
@@ -5,14 +5,39 @@ const expect = require('chai').expect;
 const Buffer = require('buffer').buffer;
 const fsReadModule = require(`${__dirname}/../lib/fs-read.js`);
 
+const testData = ['646f6c6f72204c6f', '4c6f72656d206970', '697073756d206c6f'];
+
 describe('fs-read', function(){
   describe('#readFiles', function(){
-    it('should log the text files in numerical order, regardless of size', function(){
-      fs.readFile(`${__dirname}/../data/one.txt`, function(err, data){
-        let bufferData = data;
-        expect(fsReadModule.textFiles[2]).to.equal(bufferData);
+    let files = [];
+    it('should return the first file first', function(done) {
+      fs.readFile(`${__dirname}/../data/one.txt`, function(err, data) {
+        files.push(data.toString('hex', 0, 8));
+        expect(data.toString('hex', 0, 8)).to.equal(testData[0]);
         done();
       });
+    });
+    
+    it('should return the second file second', function(done) {
+      fs.readFile(`${__dirname}/../data/two.txt`, function(err, data) {
+        files.push(data.toString('hex', 0, 8));
+        expect(data.toString('hex', 0, 8)).to.equal('4c6f72656d206970');
+        done();
+      });
+    });
+    
+    it('should return the third file last', function(done) {
+      fs.readFile(`${__dirname}/../data/three.txt`, function(err, data) {
+        files.push(data.toString('hex', 0, 8));
+        expect(data.toString('hex', 0, 8)).to.equal('697073756d206c6f');
+        done();
+      });
+    });
+    
+    it('should have the files in the correct order', function(done) {
+      fsReadModule.readFiles();
+      expect(files).to.deep.equal(testData);
+      done();
     });
   });
 });
